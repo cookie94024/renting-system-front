b26016179a29b2d117d1a17a2ca1d4b931008411
cookie94024 <yuchen880401@gmail.com>
2022-12-14 00:51:01 +0800

    feat: fetch product data from api

--- pages/products.js
+++ pages/products.js
@@ -1,49 +1,54 @@
-const products = [
-    {
-      id: 1,
-      name: 'Basic Tee',
-      href: '/productdetail',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-      imageAlt: "Front of men's Basic Tee in black.",
-      price: '$35',
-      color: 'Black',
-    },
-    // More products...
-  ]
-  
-  export default function Example() {
-    return (
-      <div className="bg-white">
-        <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-2 lg:max-w-7xl lg:px-8">
-          <h2 className="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
-  
-          <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-            {products.map((product) => (
-              <div key={product.id} className="group relative">
-                <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
-                  <img
-                    src={product.imageSrc}
-                    alt={product.imageAlt}
-                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                  />
-                </div>
-                <div className="mt-4 flex justify-between">
-                  <div>
-                    <h3 className="text-sm text-gray-700">
-                      <a href={product.href}>
-                        <span aria-hidden="true" className="absolute inset-0" />
-                        {product.name}
-                      </a>
-                    </h3>
-                    <p className="mt-1 text-sm text-gray-500">{product.color}</p>
-                  </div>
-                  <p className="text-sm font-medium text-gray-900">{product.price}</p>
-                </div>
+import axios from "axios";
+import Link from "next/link";
+import { API_BASE, DEFAULT_IMAGE } from "../constants";
+
+export default function Products({ products }) {
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-2 lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+          Products
+        </h2>
+
+        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {products.map((product) => (
+            <div key={product.id} className="group relative">
+              <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+                <img
+                  src={DEFAULT_IMAGE}
+                  alt="product image"
+                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                />
               </div>
-            ))}
-          </div>
+              <div className="mt-4 flex justify-between">
+                <div>
+                  <h3 className="text-sm text-gray-700">
+                    <Link href={`/products/${product.id}`}>
+                      <span aria-hidden="true" className="absolute inset-0" />
+                      {product.product_name}
+                    </Link>
+                  </h3>
+                </div>
+                <p className="text-sm font-medium text-gray-900">
+                  ${product.product_price}
+                </p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-    )
-  }
-  
+    </div>
+  );
+}
+
+// You should use getServerSideProps when:
+// - Only if you need to pre-render a page whose data must be fetched at request time
+export const getServerSideProps = async (ctx) => {
+  const { data: products } = await axios.get(API_BASE + "/api/product"); // your fetch function here
+
+  return {
+    props: {
+      products,
+    },
+  };
+};
