f167cfb36465df0c02be566e00ea204c4ba77ed8
cookie94024 <yuchen880401@gmail.com>
2022-12-14 02:05:58 +0800

    feat: add product filter

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,18 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 import Head from "next/head";
 import Layout from "../components/layout";
+import { QueryClientProvider, QueryClient } from "react-query";
 
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-  <Component {...pageProps} />
-  </Layout>
-  )
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </QueryClientProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/products.js
+++ pages/products.js
@@ -1,17 +1,33 @@
 import axios from "axios";
 import Link from "next/link";
+import { useState } from "react";
+import ProductFilter from "../components/ProductFilter";
 import { API_BASE, DEFAULT_IMAGE } from "../constants";
 
 export default function Products({ products }) {
+  const [currentType, setCurrentType] = useState(0);
+
+  const handleChangeType = (id) => {
+    setCurrentType(id);
+  };
+
+  const filteredProducts = products.filter((product) => {
+    if (currentType === 0) return true;
+    return product.product_type?.includes(currentType);
+  });
+
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-2 lg:max-w-7xl lg:px-8">
-        <h2 className="text-2xl font-bold tracking-tight text-gray-900">
-          Products
-        </h2>
+        <div className="flex justify-between">
+          <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+            Products
+          </h2>
+          <ProductFilter value={currentType} onChange={handleChangeType} />
+        </div>
 
         <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          {products.map((product) => (
+          {filteredProducts.map((product) => (
             <div key={product.id} className="group relative">
               <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
                 <img
