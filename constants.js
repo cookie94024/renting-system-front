33c8b0a69b1736cf1c77a34be80d99e701a3b0b5
cookie94024 <yuchen880401@gmail.com>
2022-12-26 03:13:57 +0800

    feat: update env

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-export const API_BASE = "http://localhost:8000";
+export const API_BASE = process.env.NEXT_PUBLIC_API_BASE;
 export const DEFAULT_IMAGE =
   "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg";
-export const MEDIA_BASE = "http://localhost:8000";
+export const MEDIA_BASE = process.env.NEXT_PUBLIC_API_BASE;
